36228dc8500228638ff8f627b91bf573fa74feca
Marcel Campello <marcel.campello@prafrentex.com.br>
2024-10-06 21:28:16 -0300

    Move dimDisplay into displayAndBrightness

--- modules/powerdevil.nix
+++ modules/powerdevil.nix
@@ -126,6 +126,24 @@ let
           '';
         };
       };
+
+      dimDisplay = {
+        enable = lib.mkOption {
+          type = with lib.types; nullOr bool;
+          default = null;
+          example = false;
+          description = "Enable or disable screen dimming.";
+        };
+        idleTimeout = lib.mkOption {
+          type = with lib.types; nullOr (ints.between 20 600000);
+          default = null;
+          example = 300;
+          description = ''
+            The duration (in seconds), when on ${type}, the computer must be idle
+            until the display starts dimming.
+          '';
+        };
+      };
     };
 
     otherSettings = {};
@@ -175,23 +193,6 @@ let
             timeout;
       };
     };
-    dimDisplay = {
-      enable = lib.mkOption {
-        type = with lib.types; nullOr bool;
-        default = null;
-        example = false;
-        description = "Enable or disable screen dimming.";
-      };
-      idleTimeout = lib.mkOption {
-        type = with lib.types; nullOr (ints.between 20 600000);
-        default = null;
-        example = 300;
-        description = ''
-          The duration (in seconds), when on ${type}, the computer must be idle
-          until the display starts dimming.
-        '';
-      };
-    };
   };
 
   # By the same logic as createPowerDevilOptions, we can generate the
@@ -213,13 +214,13 @@ let
       TurnOffDisplayIdleTimeoutWhenLockedSec =
         cfg.powerdevil.${optionsName}.turnOffDisplay.idleTimeoutWhenLocked;
       DimDisplayWhenIdle =
-        if (cfg.powerdevil.${optionsName}.dimDisplay.enable != null) then
-          cfg.powerdevil.${optionsName}.dimDisplay.enable
-        else if (cfg.powerdevil.${optionsName}.dimDisplay.idleTimeout != null) then
+        if (cfg.powerdevil.${optionsName}.displayAndBrightness.dimDisplay.enable != null) then
+          cfg.powerdevil.${optionsName}.displayAndBrightness.dimDisplay.enable
+        else if (cfg.powerdevil.${optionsName}.displayAndBrightness.dimDisplay.idleTimeout != null) then
           true
         else
           null;
-      DimDisplayIdleTimeoutSec = cfg.powerdevil.${optionsName}.dimDisplay.idleTimeout;
+      DimDisplayIdleTimeoutSec = cfg.powerdevil.${optionsName}.displayAndBrightness.dimDisplay.idleTimeout;
       UseProfileSpecificDisplayBrightness=
         if (cfg.powerdevil.${optionsName}.displayAndBrightness.changeScreenBrightness.enable != null) then
           cfg.powerdevil.${optionsName}.displayAndBrightness.changeScreenBrightness.enable
@@ -317,6 +318,18 @@ in
       ["programs" "plasma" "powerdevil" "lowBattery" "changeScreenBrightness"]
       ["programs" "plasma" "powerdevil" "lowBattery" "displayAndBrightness" "changeScreenBrightness"]
     )
+    (lib.mkRenamedOptionModule
+      ["programs" "plasma" "powerdevil" "AC" "dimDisplay"]
+      ["programs" "plasma" "powerdevil" "AC" "displayAndBrightness" "dimDisplay"]
+    )
+    (lib.mkRenamedOptionModule
+      ["programs" "plasma" "powerdevil" "battery" "dimDisplay"]
+      ["programs" "plasma" "powerdevil" "battery" "displayAndBrightness" "dimDisplay"]
+    )
+    (lib.mkRenamedOptionModule
+      ["programs" "plasma" "powerdevil" "lowBattery" "dimDisplay"]
+      ["programs" "plasma" "powerdevil" "lowBattery" "displayAndBrightness" "dimDisplay"]
+    )
   ];
 
   config.assertions =
@@ -338,10 +351,10 @@ in
         }
         {
           assertion = (
-            cfg.powerdevil.${type}.dimDisplay.enable != false
-            || cfg.powerdevil.${type}.dimDisplay.idleTimeout == null
+            cfg.powerdevil.${type}.displayAndBrightness.dimDisplay.enable != false
+            || cfg.powerdevil.${type}.displayAndBrightness.dimDisplay.idleTimeout == null
           );
-          message = "Cannot set programs.plasma.powerdevil.${type}.dimDisplay.idleTimeout when programs.plasma.powerdevil.${type}.dimDisplay.enable is disabled.";
+          message = "Cannot set programs.plasma.powerdevil.${type}.displayAndBrightness.dimDisplay.idleTimeout when programs.plasma.powerdevil.${type}.displayAndBrightness.dimDisplay.enable is disabled.";
         }
         {
           assertion = (
